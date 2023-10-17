7b2d795a2e471a1a82c5acfb48f994a0fdc951d9
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-17 23:24:51 +0200

    Create P09.c

---
+++ Chapter 2/P09.c
@@ -0,0 +1,6 @@
+/*
+9. Write a program that takes the length and width of a rectangular yard and the 
+length and width of a rectangular house situated in the yard. Your program 
+should compute the time required to cut the grass at the rate of two square 
+feet a second.
+*/
