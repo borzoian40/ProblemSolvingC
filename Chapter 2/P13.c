0b3588ddb7952a6ae856bcba5504912aee2bdd0d
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-17 23:31:26 +0200

    Create P13.c

---
+++ Chapter 2/P13.c
@@ -0,0 +1,8 @@
+/*
+13. Write a program that predicts how many whole sections of a batch would need 
+to be created given the number of students enrolled in the batch. Assume that 
+each section accommodates 30 students. Prompt the user to enter the number 
+of students enrolled. Echo print the number of students enrolled and then 
+display both the number of sections that will be required and the number of 
+students that will be left over.
+*/
